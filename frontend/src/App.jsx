b47c3ce52b7c43e098495fad560956749f3802a3
binits09 <binits611@gmail.com>
2025-12-09 16:26:06 +0530

    yo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import Adminlayout from './layouts/Adminlayout'
+import { Container,Row,Col,Card } from 'react-bootstrap'
+import api from './services/Api';
 
 const App = () => {
-  return <>
+ const [products,setProducts] = useState([]);
+ const [editproduct,setEditproduct] = useState([null]);
+
+ const fetchproducts = async () => {
+  try {
+    const res = await api.get('/products');
+    setProducts(res.data);
+  } catch (err) {
+    console.error(err);
+  }
+ }
+
+useEffect(() => {
+    fetchproducts();
+},[]);
+
+
+ return <>
     
+    <Adminlayout/>
+
   </>
 }
 
